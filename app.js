2ff902c1984e3324203ae6e12804389655d009c3
stevezhuravel <33463234+stevezhuravel@users.noreply.github.com>
2020-03-08 20:54:49 -0400

    blog

---
+++ app.js
@@ -0,0 +1,10 @@
+//let post = document.getElementById('myForm');
+let postIndex = document.getElementById('myForm');
+
+
+function myFunction() {
+    document.getElementById("myForm");
+    console.log(post.innerHTML);
+    postIndex.innerHTML = post.value;
+
+  } 
